150011860b0063d1a0bf886e8f8f0f60de6e85e1
phamquanganh20165755 <36791244+phamquanganh20165755@users.noreply.github.com>
2018-02-24 21:14:58 +0700

    1st

---
+++ MySolution/HelloCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string a;
+            a = @"Hello World";
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+        }
+    }
+}
