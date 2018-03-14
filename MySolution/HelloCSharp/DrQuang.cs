4eae6686dc00d172957de8020e7a783b94da0863
phamquanganh20165755 <36791244+phamquanganh20165755@users.noreply.github.com>
2018-03-14 21:41:43 +0700

    me again

--- MySolution/HelloCSharp/DrQuang.cs
+++ MySolution/HelloCSharp/DrQuang.cs
@@ -12,53 +12,38 @@ using System.Threading;
 
 namespace Quang
 {
+    public delegate void MyDelegate1(int p1);
     class TestCsharp
     {
-        public static void CallToChildThread()
-        {
-            try
-            {
-                Console.WriteLine("Bat dau Thread con!!!");
-
-                // gia su chung ta dem tu 0 toi 10
-                for (int counter = 0; counter <= 10; counter++)
-                {
-                    Thread.Sleep(500); //dung trong khoang 5 giay
-                    Console.WriteLine(counter);
-                }
-
-                Console.WriteLine("Thread con hoan thanh.");
-            }
-
-            catch (ThreadAbortException e)
-            {
-                Console.WriteLine("Thread Abort Exception!!!");
-            }
-            finally
-            {
-                Console.WriteLine("Khong the bat Thread Exception!!!");
-            }
-        }
-
         public static void Main(string[] args)
         {
-            Console.WriteLine("Da luong trong C#");
-            Console.WriteLine("Vi du minh hoa huy Thread");
-            Console.WriteLine("-------------------------------------");
-
-            ThreadStart childref = new ThreadStart(CallToChildThread);
-            Console.WriteLine("Trong Main Thread: tao Thread con.");
-            Thread childThread = new Thread(childref);
-            childThread.Start();
-
-            //dung Main Thread trong khoang 2 giay
-            Thread.Sleep(2000);
-
-            //bay gio huy thread con
-            Console.WriteLine("Trong Main Thread: huy Thread con.");
-
-            childThread.Abort();
+            Bell b = new Bell();
+            b.Alarm += AlarmHandler;
+            b.Alarm += AlarmHandler2;
+            b.OnAlarm();
             Console.ReadKey();
+        }
+
+        private static void B_Alarm(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void AlarmHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ring Ring Cuckoo!!\a");
+        }
+        static void AlarmHandler2(object sender, EventArgs e)
+        {
+            Console.WriteLine("Day di hoc de");
+        }
+    }
+    class Bell
+    {
+        public event EventHandler Alarm;
+        public void OnAlarm()
+        {
+            if (Alarm != null) Alarm(this,EventArgs.Empty);
         }
     }
 }
