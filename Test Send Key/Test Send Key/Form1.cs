ecb535a9986d89685d717a7896dd9518e525b2ff
phamquanganh20165755 <36791244+phamquanganh20165755@users.noreply.github.com>
2018-03-31 14:28:22 +0700

    31/03/2018 2:27 PM

---
+++ Test Send Key/Test Send Key/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Send_Key
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SendKeys.SendWait("a  ");
+            label1.Text = textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
