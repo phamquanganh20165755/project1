7fa1e9164c40f58bb8afab8585998eb73e5b817a
phamquanganh20165755 <36791244+phamquanganh20165755@users.noreply.github.com>
2018-04-25 11:18:48 +0700

    25/04/2018 11:18 PM

---
+++ Project1/FormGoTiengViet.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Project1
+{
+    partial class FormGoTiengViet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Designed by Quang Anh Pham";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 55);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Gõ Tiếng Việt";
+            // 
+            // FormGoTiengViet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 297);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.KeyPreview = true;
+            this.Name = "FormGoTiengViet";
+            this.Text = "Gõ Tiếng Việt (bản thử nghiệm)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
